31cf2cf5ddceaf54a0c90ffc2b60b6d5918e9fdd
programmerShinobi <shinobiprogrammer@gmail.com>
2026-04-24 11:21:02 +0700

    [update] learn : scheduled

---
+++ src/test/java/programmershinobi/spring/JobTest.java
@@ -0,0 +1,23 @@
+package programmershinobi.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class JobTest {
+
+    @Autowired
+    private Job job;
+
+    @Test
+    void job() throws InterruptedException {
+        Thread.sleep(Duration.ofSeconds(5));
+        assertEquals(2L, job.getValue());
+    }
+
+}
