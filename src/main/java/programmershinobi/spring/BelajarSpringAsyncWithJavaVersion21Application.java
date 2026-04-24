8114772eb73cebb09dd15d8a80e32be03179fc63
programmerShinobi <shinobiprogrammer@gmail.com>
2026-04-24 10:56:17 +0700

    [update] learn : enable scheduling

--- src/main/java/programmershinobi/spring/BelajarSpringAsyncWithJavaVersion21Application.java
+++ src/main/java/programmershinobi/spring/BelajarSpringAsyncWithJavaVersion21Application.java
@@ -3,8 +3,10 @@ package programmershinobi.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class BelajarSpringAsyncWithJavaVersion21Application {
 
